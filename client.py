20b2466cf48ea3882c17e1146e2f7fa38a99135e
Kobi Levi <kobilevi503@gmail.com>
2021-07-13 20:56:02 +0300

    update client requests

--- client.py
+++ client.py
@@ -3,18 +3,19 @@
 import requests
 import random
 
+# add person calls
 for x in range(10):
     features = []
-    for i in range(10):
+    for i in range(256):
         features.append(round(random.random(), 2))
 
     ploads = {'name':"Person #" + str(x), "features": features}
-
     r = requests.post('http://face-recognizer.dev/api/person/',json=ploads)
 
-for x in range(1):
+# find person calls
+for x in range(10):
     features = []
-    for x in range(10):
+    for x in range(256):
         features.append(random.random())
 
     ploads = {"features": features}
